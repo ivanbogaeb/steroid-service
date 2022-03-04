6e20b1000e35f84c31cf8c38be61c7bb92926440
ivanbogaeb <ivanbogdasaebersold@live.com.ar>
2022-03-03 23:01:17 -0300

    Network metrics

--- experimental/network.py
+++ experimental/network.py
@@ -1,12 +1,22 @@
 from flask import jsonify
-import types
 
 def usage(Hardware):
-
     response = []
-
+    sensorsLength = 0
     for connection in Hardware:
-        print(connection.Name)
-        response.append(connection.Name)
+        sensorsLength = len(connection.Sensors)
+        connection.Update()
+        response.append({
+            "name": connection.Name,
+            "usage": connection.Sensors[sensorsLength - 1].Value,
+            "speed": {
+                "download": connection.Sensors[sensorsLength - 2].Value,
+                "upload": connection.Sensors[sensorsLength - 3].Value,
+            },
+            "data": {
+                "downloaded": connection.Sensors[sensorsLength - 4].Value,
+                "uploaded": connection.Sensors[sensorsLength - 5].Value,
+            },
+        })
 
     return jsonify(response)
